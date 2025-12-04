8a17ea655a56fa1408fcd394b50aa53ba82dec23
yunxt995 <abufang35@gmail.com>
2025-12-04 22:33:22 +0800

    Update diy-z1.sh

--- diy-z1.sh
+++ diy-z1.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-
-# 添加 Helloworld 源
-echo "src-git helloworld [https://github.com/fw876/helloworld.git](https://github.com/fw876/helloworld.git)" >> "feeds.conf.default"
-
-# 添加 PassWall 源
-echo "src-git passwall_packages [https://github.com/xiaorouji/openwrt-passwall-packages.git;main](https://github.com/xiaorouji/openwrt-passwall-packages.git;main)" >> "feeds.conf.default"
-echo "src-git passwall_luci [https://github.com/xiaorouji/openwrt-passwall.git;main](https://github.com/xiaorouji/openwrt-passwall.git;main)" >> "feeds.conf.default"
-
-# 添加 OpenClash
-git clone --depth 1 [https://github.com/vernesong/OpenClash.git](https://github.com/vernesong/OpenClash.git) package/openclash
-
-# 添加 HomeProxy
-git clone --depth 1 [https://github.com/immortalwrt/homeproxy.git](https://github.com/immortalwrt/homeproxy.git) package/homeproxy
-
-# 添加 Lucky
-git clone --depth 1 [https://github.com/gdy666/luci-app-lucky.git](https://github.com/gdy666/luci-app-lucky.git) package/lucky
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth 1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+git clone --depth 1 https://github.com/gdy666/luci-app-lucky.git package/lucky
