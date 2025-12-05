7cb9b47847f17f082a2157d9b17af70bd43c48fb
yunxt995 <abufang35@gmail.com>
2025-12-05 08:08:53 +0800

    Update diy-arm.sh

--- diy-arm.sh
+++ diy-arm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# 社区修复：删除旧 golang 包，并替换为更新的版本，避免编译失败
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/OpenWrt-ARM/g' package/base-files/files/bin/config_generate
